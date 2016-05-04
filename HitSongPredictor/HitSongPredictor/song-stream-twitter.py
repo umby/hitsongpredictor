402daa5e52433af063aaf0cac828a8051004508d
Umber <umber@berkeley.edu>
2016-05-03 21:25:37 -0700

    final commit

---
+++ HitSongPredictor/HitSongPredictor/song-stream-twitter.py
@@ -0,0 +1,74 @@
+﻿"""
+Using Twitter stream API, print all the tweets in the stream containing the term "Hello" in a 1 min period
+
+"""
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from Twittercredentials import *
+from time import time,ctime
+import simplejson
+import psycopg2
+
+class StdOutListener(StreamListener):
+   
+    def __init__(self,timer):
+        self.inc = 0
+        StreamListener.__init__(self)
+        # report the start of data collection...
+        print "Gathering data at %s"%(str(ctime()))
+        self.startTime = time()
+        print "Start Time = %s"%(str(ctime()))
+        self.timer = timer
+        self.count = 0
+        self.conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
+
+    def on_data(self, data):
+        try:
+            self.endTime = time()
+            self.elapsedTime = self.endTime - self.startTime
+            if self.elapsedTime <= self.timer:
+                self.dataJson =simplejson.loads(data[:-1])
+                self.dataJsonText = self.dataJson["text"].lower()
+                self.count += 1
+
+                cur.execute("SELECT title, id, tweet_count from Songs_tweet");
+                records = cur.fetchall()
+                if len(records) > 0:
+                    self.log('found %s songs in tweet_count table, before update:' % (len(records)))
+                    for rec in records:
+                        uTitle = rec[0]
+                        uId = rec[1]
+                        uCount = rec[2]
+                        if uTitle.lower() in self.dataJsonText.lower():
+                            uCount = uCount + 1
+                            cur.execute("UPDATE tweet_count SET count=%s WHERE id=%s", (uCount, uId));
+                            self.conn.commit()
+                            self.log('found %s songs in tweet_count table, before update:' % (len(records)))
+                            print self.dataJsonText
+
+            else:
+                print "Count== ",self.count
+                print "End Time = %s"%(str(ctime()))
+                print "Elapsed Time = %s"%(str(self.elapsedTime))
+
+
+
+                return False
+            return True
+        except Exception, e:
+            # Catch any unicode errors while printing to console
+            # and just ignore them to avoid breaking application.
+            pass
+
+    def on_error(self, status):
+        print ("ERROR :",status)
+
+if __name__ == '__main__':
+    # to collect the data for 1 min
+    l = StdOutListener(60)
+    mystream = tweepy.Stream(auth, l, timeout=60)
+    mystream.sample()
+
+
+
---
+++ HitSongPredictor/HitSongPredictor/wordcount.py
@@ -0,0 +1,56 @@
+﻿from __future__ import absolute_import, print_function, unicode_literals
+
+from collections import Counter
+from streamparse.bolt import Bolt
+
+import psycopg2
+
+class WordCounter(Bolt):
+
+    def initialize(self, conf, ctx):
+        self.counts = Counter()
+        self.conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
+
+
+    def process(self, tup):
+        word = tup.values[0]
+
+        # Write codes to increment the word count in Postgres
+        # Use psycopg to interact with Postgres
+        # Database name: postgres 
+        # Table name: Tweetwordcount 
+        # you need to create both the database and the table in advance.
+        cur = self.conn.cursor()
+
+        uWord = word
+        uCount = self.counts[word] + 1
+
+        cur.execute("SELECT title, tweet_count FROM Songs_tweet WHERE title=%s",[uWord]);
+        records = cur.fetchall()
+        #update or insert
+        if len(records) > 0:
+            self.log('found %s records, before update:' % (len(records)))
+            for rec in records:
+                self.log('title = %s' % (rec[0]))
+                self.log('tweet_count = %s' % (rec[1]))
+            cur.execute("UPDATE Songs_tweet SET tweet_count=%s WHERE title=%s", (uCount + rec[1], uWord));
+            self.conn.commit()
+
+			
+        #Select
+        cur.execute("SELECT title, tweet_count FROM Songs_tweet WHERE title=%s",[uWord]);
+        records = cur.fetchall()
+        self.log('found %s records, after update:' % (len(records)))
+        for rec in records:
+            self.log('title = %s' % (rec[0]))
+            self.log('tweet_count = %s' % (rec[1]))
+        
+
+        # Increment the local count
+        self.counts[word] += 1
+        self.emit([word, self.counts[word]])
+
+        # Log the count - just to see the topology running
+        self.log('%s: %d' % (word, self.counts[word]))
+		
+		
